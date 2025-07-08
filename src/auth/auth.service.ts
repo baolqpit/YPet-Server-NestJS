8401ecba90ba177413932b7d0b5c3fad14dc882e
Lê Phạm Quốc Bảo <101452469+baolqpit@users.noreply.github.com>
2025-07-08 14:04:06 +0700

    fix(auth): check existed email before create user and split email with credential before login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from '../users/dto/requests/create-user.dto';
@@ -7,6 +11,7 @@ import { LoginDto } from './dto/requests/login.dto';
 import * as bcrypt from 'bcrypt';
 import { plainToInstance } from 'class-transformer';
 import { ResponseUserDto } from '../users/dto/responses/response-user.dto';
+import { ResponseError } from '../common/enums/response-error.enum';
 
 @Injectable()
 export class AuthService {
@@ -16,6 +21,11 @@ export class AuthService {
   ) {}
 
   async register(dto: CreateUserDto): Promise<AuthResponse> {
+    const emailExists = await this.userService.findByEmail(dto.email);
+    if (emailExists) {
+      throw new ConflictException(ResponseError.EMAIL_ALREADY_EXISTS);
+    }
+
     const user = await this.userService.create(dto);
     const token = this.jwtService.sign({ sub: user._id, email: user.email });
 
@@ -28,9 +38,19 @@ export class AuthService {
   async login(dto: LoginDto): Promise<AuthResponse> {
     const userDoc = await this.userService.findByEmail(dto.email);
 
-    if (!userDoc || !(await bcrypt.compare(dto.password, userDoc.password))) {
-      // throw new UnauthorizedException('Invalid Credentials');
-      throw new UnauthorizedException();
+    ///Email user chưa tồn tại trong hệ thống
+    if (!userDoc) {
+      throw new UnauthorizedException(ResponseError.USER_NOT_FOUND);
+    }
+
+    const passwordMatched = await bcrypt.compare(
+      dto.password,
+      userDoc.password,
+    );
+
+    ///Thông tin đăng nhập sai
+    if (!passwordMatched) {
+      throw new UnauthorizedException(ResponseError.INVALID_CREDENTIALS);
     }
 
     const user = plainToInstance(ResponseUserDto, userDoc, {
